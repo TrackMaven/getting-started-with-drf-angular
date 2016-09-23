9237c83a1473615d855d338b9eaeb47b7bcb8ca6
Tym <tim.butler@trackmaven.com>
2016-09-23 15:26:17 -0400

    Added services

--- client/public/components/retail/controllers/retail.control.js
+++ client/public/components/retail/controllers/retail.control.js
@@ -1,8 +1,14 @@
 retail
-    .controller('RetailController', function($scope, chainFactory) {
+    .controller('RetailController', function($scope, chainFactory, storeFactory, employeeFactory) {
         chainFactory.get().then(function(result) {
             $scope.chains = result.data
-        })
+        });
 
-        $scope.message = "Hello World"
+        storeFactory.get().then(function(result) {
+            $scope.stores = result.data
+        });
+
+        employeeFactory.get().then(function(result) {
+            $scope.employees = result.data
+        });
 });
--- client/public/components/retail/services/retailApi.service.js
+++ client/public/components/retail/services/retailApi.service.js
@@ -8,4 +8,28 @@ retail
                 })
             }
         }
+    }])
+
+retail
+    .factory('storeFactory', ['$http', function($http) {
+        return {
+            get : function() {
+                return $http({
+                    method: 'GET',
+                    url: 'http://localhost:8000/stores/'
+                })
+            }
+        }
+    }])
+
+retail
+    .factory('employeeFactory', ['$http', function($http) {
+        return {
+            get : function() {
+                return $http({
+                    method: 'GET',
+                    url: 'http://localhost:8000/employees/'
+                })
+            }
+        }
     }]);
